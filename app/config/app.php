d4a847b1c7f97dfa4ddc8344139db2d7c81b1642
Sean Anderson <wing5wong@hotmail.com>
2014-01-26 23:38:33 +1300

    remove barryvdh debugbar

--- app/config/app.php
+++ app/config/app.php
@@ -107,7 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Barryvdh\Debugbar\ServiceProvider',
+		//'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 
